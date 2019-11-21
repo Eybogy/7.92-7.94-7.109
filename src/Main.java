a06ef840da66e0a3ef189d2bd57a35025fc26ce4
Grish <nightsalat@gmail.com>
2019-11-21 21:32:19 +0300

    Did task 7.92

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        One a = new One();
+        a.multiplication();
+
+
+    }
 }
--- src/One.java
+++ src/One.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 /* Задача 7.92. Напишите программу, перемножающую два натураль-
 ных числа, которая не использует операции умножения. */
 
 public class One {
+    public static Integer count(int n1, int n2, int counter) {
+        if (n2 != 0){
+            return n2;
+        }
+        return 1;
+    }
+
+    static void multiplication() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int n1 = in.nextInt();
+        int counter = n1;
+        System.out.println("Введите второе число");
+        int n2 = in.nextInt();
+        int result = 0;
+        while (counter != 0) {
+            result += count(n1, n2, counter);
+            counter -= 1;
+        }
+        System.out.println(result);
+    }
 }
